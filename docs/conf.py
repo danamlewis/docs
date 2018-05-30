fe467e4eb909ae59044b0796c5c2df5f5fd07a5b
Dana Lewis <danamlewis@users.noreply.github.com>
2018-05-30 09:48:41 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ release = u'0.0.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = de
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
